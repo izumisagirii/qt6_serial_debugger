7293d9efcdebb8a0c4c2f9849b25aa38233458bc
Yongheng CUI <cuiyongheng1@outlook.com>
2024-01-15 16:06:20 +0800

    update

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ auto main(int argc, char *argv[]) -> int {
   QApplication a(argc, argv);
   MainWindow w;
   w.setWindowTitle("Protocol Debugger");
-  w.setWindowIcon(QIcon("./icon.ico"));
+  // w.setWindowIcon(QIcon("./favicon.ico"));
   w.show();
   return QApplication::exec();
 }
